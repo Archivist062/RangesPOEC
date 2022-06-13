93d8faa68e207a8e4c1db490bb3faea3875735a6
Ludovic Lagouardette <ludovic@nekoit.xyz>
2022-06-13 12:02:27 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ranges
+{
+    class Program
+    {
+        class Truc : IComparable<Truc>
+        {
+            int f;
+            int IComparable<Truc>.CompareTo(Truc other)
+            {
+                return f - other.f;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine((new Range(1, 5000)).Aggregate((x, y) => x + y));
+
+            var range = new Range(1, 10);
+
+            foreach(var i in range)
+            {
+                foreach(var j in range)
+                {
+                    Console.WriteLine(i * j);
+                }
+            }
+        }
+    }
+}
---
+++ Range.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ranges
+{
+
+    public class RangeEnumerator : IEnumerator<int>
+    {
+        public RangeEnumerator(Range t)
+        {
+            theRange = t;
+            current = theRange.depart - theRange.direction;
+        }
+
+        Range theRange;
+        int current;
+
+        int IEnumerator<int>.Current => current;
+
+        object System.Collections.IEnumerator.Current => current;
+
+        public void Dispose()
+        { }
+
+        public bool MoveNext()
+        {
+            if (current == theRange.arrivee - theRange.direction)
+            {
+                return false;
+            }
+            current += theRange.direction;
+            return true;
+        }
+
+        public void Reset()
+        {
+            current = theRange.depart - theRange.direction;
+        }
+    }
+
+    public class Range : IEnumerable<int>
+    {
+        internal int depart;
+        internal int arrivee;
+        internal int direction;
+
+        public Range(int depart, int arrivee)
+        {
+            this.depart = depart;
+            this.arrivee = arrivee;
+            if (depart < arrivee)
+            {
+                direction = +1;
+            }
+            else
+            {
+                direction = -1;
+            }
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new RangeEnumerator(this);
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return new RangeEnumerator(this);
+        }
+    }
+}
